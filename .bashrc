793f90a06bd19622983cd1836cfe65812ebe5299
Nils <40796807+uchars@users.noreply.github.com>
2024-09-13 20:33:33 +0200

    alias and ps1

--- .bashrc
+++ .bashrc
@@ -1,1 +1,8 @@
 export PATH="$PATH:$HOME/.local/bin/nvim/bin:$HOME/.local/bin"
+if [ -n "$TERM" ] && [ "$TERM" != "dumb" ]; then
+  export PS1='\[\033[32m\]\u@\h\[\033[0m\](\w)> '
+else
+      export PS1='\u@\h:\w\$ '
+fi
+
+source "$HOME/.config/alias.sh"
---
+++ .config/alias.sh
@@ -0,0 +1,25 @@
+# editor
+alias v="nvim"
+alias nv="nvim"
+
+# git
+alias gs="git status"
+alias gf="git fetch --all"
+alias gwip='git add -A; git rm $(git ls-files --deleted) 2> /dev/null; git commit -m "[WIP]: $(date)"'
+alias gundo="git reset HEAD~"
+
+# apt
+alias agi="sudo apt-get install "
+
+# finding
+alias p="ps aux | grep -i "
+alias h="history | grep -i "
+alias diskspace="du -S | sort -nr | more"
+
+# list
+alias ll="ls -al"
+alias la="ls -A"
+
+# keyboard
+alias kger="setxkbmap de"
+alias kus="setxkbmap us"
